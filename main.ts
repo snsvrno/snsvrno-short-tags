6e14062a820892d228c1cffa1cee8557068697af
snsvrno <snsvrno@tuta.io>
2023-03-01 16:05:53 -0700

    init: inital commit, basic version
    the inital commit translating the hacky test .js file to a .ts project.
    
    * adds setting to show / hide the "#"
    * adds setting to trim a tag if matches on of the hide tags
    * adds setting to easily add multiple hide tags

--- main.ts
+++ main.ts
@@ -1,78 +1,40 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+// SETTINGS ///////////////////////////
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SnsvrnoSettings {
+	tags: Array<string>;
+	showHash: Bool;
+	showOneWord: Bool;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SnsvrnoTagsSettings = {
+	tags: [ ],
+	showHash: true,
+	showOneWord: true,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// PLUGINS //////////////////////////////
+
+export default class SnsvrnoTags extends Plugin {
+	settings: SnsvrnoTagsSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SnsvrnoTagsSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		/*this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
-		});
+		});*/
+
+	 // checks every tag and runs the formatter on it
+	 this.registerMarkdownPostProcessor((element) => {
+			element.querySelectorAll("a.tag").forEach((a) => this.formatTag(a));
+	 });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -89,28 +51,56 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	/**
+	 * checks if the tag fits any of the defined shortening requirements
+	 * and then shortens it
+	 */
+	formatTag(element) {
+
+		var name = element.text;
+		if (name.substring(0,1) == "#") name = name.substring(1);
+
+		// works through the list of tags
+		for (var i = 0; i < this.settings.tags.length; i++) {
+			var tag = this.settings.tags[i];
+			if (name.length >= tag.length && name.substring(0,tag.length) == tag) {
+
+				// if we have the same tag but we set the option to trucate it.
+				if (name == tag && this.settings.showOneWord) {
+					var parts = name.split("/");
+					name = parts.pop();
+				}
+				// if this is the same tag, we will not remove it
+				else if (name == tag) break;
+				// otherwise we trim it
+				else {
+					var pos = tag.length;
+					// checking if the setting ended in "/", because otherwise
+					// we need to add 1 in order to remove it.
+					if (tag.substring(tag.length-1,1) != "/") pos += 1;
+					name = name.substring(pos);
+				}
+			}
+		}
+	
+		// adds back the hash if the settings ask for it.
+		if (this.settings.showHash) name = "#" + name;
+
+		element.dataset.uri = `obsidian://search?query=tag:${encodeURIComponent(element.text)}`;
+		element.text = name;
+		element.target = "_blank";
+		element.rel = "noopener";
+    element.onclick = () => window.open(element.dataset.uri);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// SETTINGS TAB ////////////////////////
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class SnsvrnoTagsSettingTab extends PluginSettingTab {
+	plugin: SnsvrnoTags;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SnsvrnoTags) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +110,61 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'General Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Show Hash')
+			.setDesc('Show the hash/pound for tags when in reading mode.')
+			.addToggle(toggle =>
+				toggle.setValue(this.plugin.settings.showHash)
+					.onChange(async (value) => {
+						this.plugin.settings.showHash = value;
+						this.plugin.saveSettings();
+					}));
+
+		new Setting(containerEl)
+			.setName('Trim One Word on Matching')
+			.setDesc('If the short tag name is the same as the tag being use, truncate the tag and only show the last tag. If disabled the entire tag will be shown.')
+			.addToggle(toggle =>
+				toggle.setValue(this.plugin.settings.showOneWord)
+					.onChange(async (value) => {
+						this.plugin.settings.showOneWord = value;
+						this.plugin.saveSettings();
+					}));
+
+		/////////////////////////////////////////////
+
+		new Setting(containerEl)
+			.setName('Tags')
+			.setDesc('List of full tag parents that will be shortened / hidden in reading mode.');
+
+		this.plugin.settings.tags.forEach((t) => {
+			var index = this.plugin.settings.tags.indexOf(t);
+			new Setting(containerEl)
+				.addText(text =>
+					text.setValue(t)
+						.onChange(async (v) => {
+							this.plugin.settings.tags[index] = v;
+							this.plugin.saveSettings();
+						}))
+				.addButton((b) => {
+					b.setButtonText("Remove");
+					b.setTooltip("Remove Tag '" + t + "'");
+					b.onClick(async (_) => {
+						this.plugin.settings.tags.splice(index,1);
+						this.display();
+					});
+				});
+		});
+
+		new Setting(containerEl)
+			.addButton((b) => {
+				b.setButtonText("Add");
+				b.setTooltip("Add new tag");
+				b.onClick(async (_) => {
+					this.plugin.settings.tags.push("tag/parents/here");
+					this.display();
+				});
+			});
 	}
 }
